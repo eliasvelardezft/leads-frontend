af70a5333f6a13677abc789220fe60a8846660f9
Elias Velardez <elias.velardez@bigbox.com.ar>
2023-10-18 02:40:26 -0300

    fix: css alignment

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,11 +2,13 @@
     <div class="flex items-center justify-center min-h-screen bg-gray-900">
         <div class="w-full max-w-md p-8 bg-white rounded shadow-md flex flex-col items-center">
             <h1 class="mb-4 text-2xl font-semibold text-gray-700">Home</h1>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full mb-4">
-                <router-link to="/register-lead" class="text-white">Register lead</router-link>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full mb-4 relative">
+                <router-link to="/register-lead" class="block absolute inset-0"></router-link>
+                Register lead
             </button>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
-                <router-link to="/enroll" class="text-white">Enroll</router-link>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full relative">
+                <router-link to="/enroll" class="block absolute inset-0"></router-link>
+                Enroll
             </button>
         </div>
     </div>
--- src/components/enrollment/EnrollmentRegistration.vue
+++ src/components/enrollment/EnrollmentRegistration.vue
@@ -23,42 +23,22 @@ const handleSubmit = async () => {
 
 </script>
 
-<!-- <template>
-    <form v-if="!isSubmitted" @submit.prevent="handleSubmit">
-        <label for="lead-id">Lead ID:</label>
-        <input type="text" id="lead-id" v-model="leadId">
-        <EnrollmentForm v-for="(enrollment, index) in enrollments" :key="index" v-model="enrollments[index]" />
-        <button type="button" @click="addEnrollment">Add Another Enrollment</button>
-        <button type="submit">Submit</button>
-    </form>
-    <div v-else>
-        <h1>Enrollments Submitted</h1>
-        <div v-for="(enrollment, index) in enrollments" :key="index">
-            <p>------------------</p>
-            <p>Lead ID: {{ leadId }}</p>
-            <p>Career ID: {{ enrollment.career.name }}</p>
-            <p>Subject ID: {{ enrollment.subject.name }}</p>
-            <p>Times taken: {{ enrollment.subjectTimesTaken }}</p>
-            <p>classroom: {{ enrollment.course.classroom }}</p>
-            <p>professor: {{ enrollment.course.professor }}</p>
-            <p>starts: {{ enrollment.course.start_date }}</p>
-            <p>ends: {{ enrollment.course.end_date }}</p>
-            <p>------------------</p>
-        </div>
-    </div>
-</template> -->
-
 <template>
     <div class="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-gray-50">
-        <form v-if="!isSubmitted" @submit.prevent="handleSubmit" class="bg-gray-800 p-4 rounded-lg mx-auto w-3/4 mt-10">
+        <h3 v-if="error" class="text-red-500">Error: {{ error }}</h3>
+        <form v-if="!isSubmitted" @submit.prevent="handleSubmit"
+            class="bg-gray-800 p-4 rounded-lg mx-auto w-3/4 mt-10 flex flex-col">
             <label for="lead-id" class="block text-sm font-medium">Lead ID:</label>
             <input type="text" id="lead-id" v-model="leadId" class="bg-gray-50 text-gray-900 p-2 rounded w-full mb-4">
             <EnrollmentForm v-for="(enrollment, index) in enrollments" :key="index" v-model="enrollments[index]" />
-            <button type="button" @click="addEnrollment"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4">Add Another
-                Enrollment</button>
-            <button type="submit"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+            <div class="flex justify-center">
+                <button type="button" @click="addEnrollment"
+                    class="flex-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center mx-2">Add
+                    Another
+                    Enrollment</button>
+                <button type="submit"
+                    class="flex-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center mx-2">Submit</button>
+            </div>
         </form>
         <div v-else class="w-full max-w-md p-8 bg-white rounded shadow-md flex flex-col items-center">
             <h1 class="mb-4 text-2xl font-semibold text-gray-700">Enrollments Submitted</h1>
